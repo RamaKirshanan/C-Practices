d9a6716c7583d233181107e8238db9227c7b083a
RamaKrishan <ramamass23@gmail.com>
2026-06-19 02:54:45 +0530

    star printing

---
+++ C - Practice/Array/star-printing-practice.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,n;
+    printf("enter n:");
+    
+
+    for (i = 1; i <= n; i++)
+
+    {
+        for (j = n; j <= 1; j--)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
