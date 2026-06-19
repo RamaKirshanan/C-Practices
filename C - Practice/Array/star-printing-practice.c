329da70cc857e948d8ed30eb56285ec51a21f9e6
RamaKrishan <ramamass23@gmail.com>
2026-06-19 10:13:33 +0530

    printing c

--- C - Practice/Array/star-printing-practice.c
+++ C - Practice/Array/star-printing-practice.c
@@ -3,12 +3,12 @@ int main()
 {
     int i,j,n;
     printf("enter n:");
-    
+    scanf("%d",&n);
 
     for (i = 1; i <= n; i++)
 
     {
-        for (j = n; j <= 1; j--)
+        for (j = i; j <=n ; j++)
         {
             printf("*");
         }
