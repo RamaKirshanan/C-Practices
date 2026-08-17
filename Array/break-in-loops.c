6d634f491020f8b79e640ee4a2e937059adebcb1
RamaKirshnan <ramamass23@gmail.com>
2026-08-17 23:13:57 +0530

    rama

--- Array/break-in-loops.c
+++ Array/break-in-loops.c
@@ -15,7 +15,64 @@ int main()
         }sum=sum+a;
     }printf("sum=%d",sum);
 }
+/*
+
+nee ipo pannu , paklam enna panana
+intha 3 commetns tha push paanna
 
 // git add .
 // git commit -m "commit message"
 // git push
+
+git add .
+git commit -m "Edho oru message "
+git pushhmmmmm
+
+terminal open panni push pannuu....
+
+C-pratices ulla irunthu tha push pannum... punda atha mistake ka tha ivlo neram panidu iruthen,.....
+
+C-Practices ulla irunthu push pannum puriyutha rama aaa?
+done ra rama...
+sinna sinna mistake... 
+
+ipo namma code  yellam C-Practices folder ulla tha irukku, so nama ahutkku ulla poi push pannum
+apo en python files , yellame C-practices ulla tha irukku,,,
+
+nee athu ulla poi tha push pannanum.. enga pannu 
+ok paru
+
+
+thappa na folder la irunthu push pannidu irukken yella time mum shit...
+
+terminal la ipdi irukanum... "D:\new one\C-Pratices>" ok va?hmm
+
+ok ipo neeye push pannu..
+
+sily mistake...
+
+C-pratices than correct folder, push pannurathukku...
+
+simple ra chi, itha kandupida loosu punda mari chai... 
+
+regret... chai... othe sinna ithu da... chai.. otha sinna oru folder changes ra.. chii
+
+nee ye paru github la,  antha c-pratics ulla poi 3 line type panna podum
+
+ivlo neram vera folder ra irunthu pannidu iruthe
+
+chai chai chai...
+
+
+ok va ???? nanba ipo ennala ingayeh folder open panna mudithyu
+
+mudiyume...  enna solla vara... ?pera ean nee file manager la irynthu vara __stdio_common_vfscanf
+athu easy ya irukkum.. avlothan..
+
+inga ve  koda open panna la... 
+alothan... okkkkkkkkk
+
+ipo 
+
+*/
+
